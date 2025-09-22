9ca185cee92ae68a612349e4302047ad3533880f
Asvin Goel <github@telematique.eu>
2025-09-22 21:17:16 +0200

    Rm breakout.jl

--- julia/08-lecture/breakout/run_game.jl
+++ julia/08-lecture/breakout/run_game.jl
@@ -42,9 +42,61 @@ function breakout(control_file="08-lecture/breakout/control/keyboard.jl")
             Pkg.instantiate()
         end
         
-        ENV["CONTROL_MODULE"] = control_path
-        breakout_file = joinpath(breakout_dir, "breakout.jl")
-        rungame(breakout_file)
+        cd(breakout_dir)
+        
+        # Create temporary game file with control path embedded
+        temp_game_content = """
+        using GameZero
+        using FileIO, Images
+
+        # Load game modules
+        game_include("core/digits.jl")
+        game_include("core/game_logic.jl")
+        game_include("core/screenshot.jl")
+        game_include("core/display.jl")
+        game_include("$control_path")
+
+        # Import modules
+        using .Digits
+        using .GameLogic
+        using .Screenshot
+        using .Display
+        using .Control
+
+        # GameZero callbacks
+        function draw(g)
+            draw_game(g)
+        end
+
+        function update(g)
+            action = get_action(g)
+            handle_input(action)
+            update_physics()
+        end
+
+        function on_key_down(g, key, scancode)
+            if key == GameZero.Keys.ESCAPE
+                GameZero.playing[] = false
+            elseif key == GameZero.Keys.P
+                game_state = get_game_state()
+                pixels = screenshot(game_state)
+                filename = "breakout_state_\$(round(Int, time())).png"
+                save(filename, Gray.(pixels))
+                println("Pixel state saved as \$filename")
+            end
+        end
+        """
+        
+        # Write and run temporary game file
+        temp_file = joinpath(breakout_dir, "temp_game.jl")
+        write(temp_file, temp_game_content)
+        
+        try
+            GameZero.rungame(temp_file)
+        finally
+            rm(temp_file, force=true)
+        end
+        
     finally
         # Switch back to original environment and directory
         Pkg.activate(original_env)
